6593ad94e4c3651f56e6ca76d6ba89ba93fd1cd2
miguelbitz <miguelbitz@gmail.com>
2023-10-03 17:48:56 -0300

    deploy ajustments

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,21 @@
 import express from 'express'
 import cors from 'cors'
+import dotenv from 'dotenv'
 import { userRouter } from './router/userRouter'
 import { postRouter } from './router/postRouter'
 import { likeDislikePostRouter } from './router/likeDislikePostRouter'
 import { commentsRouter } from './router/commentsRouter'
 import { likeDislikeCommentRouter } from './router/likeDislikeCommentRouter'
 
+dotenv.config()
+
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-app.listen(3003, () => {
-    console.log(`Servidor rodando na porta ${3003}`)
+app.listen(Number(process.env.PORT) || 3003, () => {
+    console.log(`Servidor rodando na porta ${Number(process.env.PORT) || 3003}`)
 })
 
 app.use("/users", userRouter)
